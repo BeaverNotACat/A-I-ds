1b8f93489bd264b311f7b2504e3f38bffd6f3396
BeaverNotACat <beavernotacat@gmail.com>
2024-07-05 08:51:25 +0000

    🎨 Update prroject structure

--- backend/app/api/example.py
+++ backend/app/api/example.py
@@ -1,6 +1,4 @@
 from litestar import Router, get
-from litestar.contrib.htmx.request import HTMXRequest
-from litestar.contrib.htmx.response import HTMXTemplate
 from litestar.response import Template
 
 from tensorflow import Module  # type: ignore[import-untyped]
@@ -11,8 +9,8 @@ from app.storage.unit_of_work import UnitOfWork
 @get(
     path="/example",
 )
-def example(request: HTMXRequest, uow: UnitOfWork, model: Module) -> Template:
-    return HTMXTemplate(template_name="main.html")
+def example(uow: UnitOfWork, model: Module) -> str:
+    return "Hello, world!"
 
 
 example_router = Router(
--- backend/app/app.py
+++ backend/app/app.py
@@ -1,13 +1,9 @@
-from pathlib import Path
-
 from litestar import Litestar
 from litestar.di import Provide
-from litestar.contrib.jinja import JinjaTemplateEngine
-from litestar.template.config import TemplateConfig
 
 from app.state import model_setup, unit_of_work_setup
 from app.utils.dependencies import unit_of_work_dependencie, model_dependencie
-from app.views.example import example_router
+from app.api.example import example_router
 
 app = Litestar(
     lifespan=[model_setup, unit_of_work_setup],
@@ -15,9 +11,5 @@ app = Litestar(
         "uow": Provide(unit_of_work_dependencie),
         "model": Provide(model_dependencie)
     },
-    template_config=TemplateConfig(
-        directory=Path("app/templates"),
-        engine=JinjaTemplateEngine,
-    ),
     route_handlers=[example_router]
 )
